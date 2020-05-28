cd83d05b15aa517d4f902cc403daed92c0acd9f3
WON KIM <kimws@cicomprogram.com>
2020-05-28 08:14:03 -0700

    Vue.js sample code

---
+++ VueApp01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VueApp01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
